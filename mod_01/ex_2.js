545a8cd426391154b2be920d5b4f3662836d88b7
jesssielena <jessicaelenadacosta@hotmail.com>
2022-01-21 16:10:07 -0300

    exercicio 2 do módulo 1 concluído

--- mod_01/ex_2.js
+++ mod_01/ex_2.js
@@ -1,1 +1,20 @@
-console.log("Segundo exer");
+// Neste problema, deve-se ler o nome de uma peça que chamaremos de peça1, 
+// o número de peças1 que o usuário quer, o valor unitário de cada peça1, o nome de uma peça2, 
+// o número de peças2 e o valor unitário de cada peça2. Após, calcule e mostre o valor a ser pago.
+
+
+const prompt = require('prompt-sync')();
+
+const peca1 = prompt('Digite o nome da peça 1:');
+const valorPeca1 = parseFloat(prompt('Digite o valor unitário da peça 1: '));
+const qtPeca1 = +prompt('Digite a quantidade de peças: ');
+
+
+const peca2 = prompt('Digite o nome da peça 1:');
+const valorPeca2 = parseFloat(prompt('Digite o valor unitário da peça 1: '));
+const qtPeca2 = +prompt('Digite a quantidade de peças: ');
+
+
+const valorTotal = (valorPeca1*qtPeca1) + (valorPeca2*qtPeca2);
+
+console.log(`O valor a ser pago é R$ ${valorTotal}`);
